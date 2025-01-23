24899b162ef802d650e9c5a21e61b88dfa5d6e60
Tawanda Brandon <work@tawandabrandon.co>
2025-01-23 06:00:47 +0200

    refactor: move Redis credentials to environment variables
    -

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,6 +1,9 @@
-import { connect } from "redis";
+import { connect } from "https://deno.land/x/redis@v0.32.1/mod.ts";
 
 export const redis = await connect({
-  hostname: "127.0.0.1",
-  port: 6379,
+  hostname: Deno.env.get("REDIS_HOSTNAME") || "",
+  port: Number(Deno.env.get("REDIS_PORT")) || 6379,
+  username: Deno.env.get("REDIS_USERNAME") || "",
+  password: Deno.env.get("REDIS_PASSWORD") || "",
+  tls: true,
 });
